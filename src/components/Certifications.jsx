a58f21aec39ee84b7d26611feb4095d51c3f6f4c
Abdul Azeem Sheikh <151495014+azzu-sheikh@users.noreply.github.com>
2025-12-19 23:14:40 +0530

    Update certifications lists

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -12,10 +12,10 @@ export default function Certifications() {
       link: "https://drive.google.com/file/d/1HWdmyzHbiNeof6SniS7nreDq5qEHNh89/view?usp=drivesdk" // PASTE LINK HERE
     },
     { 
-      title: "Cyber Security", 
-      issuer: "IBM - Edunet Foundation", 
-      date: "25 Nov 2023",
-      link: "https://drive.google.com/file/d/1gePTY-ZVQwB3UwxrLm_AXItzj3TPBgGE/view?usp=drivesdk"
+      title: "Python Programming", 
+      issuer: "I360DigiTMG", 
+      date: "28 Aug 2025",
+      link: "https://drive.google.com/file/d/1H_Nc8lCiQttb_2asiDL6QDQOrEepZ-5P/view?usp=sharing"
     },
     { 
       title: "Geo Data Processing using Python", 
@@ -24,16 +24,34 @@ export default function Certifications() {
       link: "https://drive.google.com/file/d/1gkZWQF4BHqq1pCzIdDMShMJL2YxH7bSE/view?usp=drivesdk" // PASTE LINK HERE
     },
     { 
+      title: "Python Programming", 
+      issuer: "Coursera", 
+      date: "15 July 2023",
+      link: "https://drive.google.com/file/d/1rjcMGIOhnGoves8IH1dwts3qJL6c2-Dj/view?usp=sharing" // PASTE LINK HERE
+    },
+    { 
+      title: "Cyber Security", 
+      issuer: "IBM - Edunet Foundation", 
+      date: "25 Nov 2023",
+      link: "https://drive.google.com/file/d/1gePTY-ZVQwB3UwxrLm_AXItzj3TPBgGE/view?usp=drivesdk"
+    },
+     { 
+      title: "ChatGPT & AI Tools", 
+      issuer: "Skill Nation", 
+      date: "03 July 2024",
+      link: "https://drive.google.com/file/d/1jWah_Fs5qTO6sM1VQc8z2ruai8zFabin/view?usp=drivesdk" // PASTE LINK HERE
+    },
+    { 
       title: "Computational Theory", 
       issuer: "Infosys Spring Board", 
       date: "14 Dec 2023",
       link: "https://drive.google.com/file/d/1gwSayDfOIwcwTuubSB_lqtU6HbRLEjoL/view?usp=drivesdk" // PASTE LINK HERE
     },
     { 
-      title: "ChatGPT & AI Tools", 
-      issuer: "Skill Nation", 
-      date: "03 July 2024",
-      link: "https://drive.google.com/file/d/1jWah_Fs5qTO6sM1VQc8z2ruai8zFabin/view?usp=drivesdk" // PASTE LINK HERE
+      title: "THE IAMAI FUTURE FOUNDERS 2022", 
+      issuer: "K-Tech & IAMAI", 
+      date: "21 Oct 2022",
+      link: "https://drive.google.com/file/d/10Cxv3KBbZGHCkuqI_LUvzfyMr5ztpymc/view?usp=drive_link" // PASTE LINK HERE
     },
     { 
       title: "Design Impact Movement", 
@@ -112,4 +130,5 @@ export default function Certifications() {
       </motion.ul>
     </section>
   );
+
 }
