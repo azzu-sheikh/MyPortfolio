b74ffdd2c167bbd6e2f338d71ceadad9bcd464cb
Abdul Azeem Sheikh <151495014+azzu-sheikh@users.noreply.github.com>
2026-03-24 20:15:45 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ export default function Projects() {
     {
   title: "Multi-Modal Agentic AI Assistant",
   description: "Built a scalable Agentic AI system that replaces single-model dependency with dynamic multi-model routing. Achieves ultra-low latency using lightweight Hugging Face models, supports tool execution, memory, and is fully optimized for real-time deployment on Streamlit Cloud.",
-  tech: ["Agentic AI Architecture", "Large Language Models (LLMs)", "LangChain & LangGraph", "Hugging Face Inference API", "Model Routing & Orchestration", "Retrieval-Augmented Generation (RAG)", "Streamlit (Production UI)", "Python (Backend Engineering)", "Vector Databases (FAISS/Chroma)","Prompt Engineering" ],
+  tech: ["Agentic AI Architecture", "LLMs", "LangChain & LangGraph", "Hugging Face", "RAG", "Streamlit", "Python", "VectorDB","Prompt Engineering" ],
   link: "https://github.com/azzu-sheikh/Multi-Modal-Agentic-AI-Assistant",
   demo: "#"
 },
